cb73c116fc7696f46804013d3c3b1fa3803f1077
Lorenzo-Arnau <skyline03@hotmail.it>
2021-01-12 14:41:30 +0100

    exercise done and works..in all versions

---
+++ main.js
@@ -0,0 +1,47 @@
+// function appear(target){
+//   $(target).show();
+// }
+// function disappear(target){
+//   $(target).hide();
+// }
+
+// un click solo senza funzioni
+// $( '.fa-bars' ).click(function() {
+//  $('.hamburger-menu').show();
+// });
+// $( '.fa-times' ).click(function() {
+//  $('.hamburger-menu').hide();
+// });
+
+
+// un click solo con funzioni
+// $( '.fa-bars' ).click(function() {
+//   appear('.hamburger-menu');
+// });
+// $( '.fa-times' ).click(function() {
+//   disappear('.hamburger-menu');
+// });
+
+//due click
+// $('.fa-bars').dblclick(function() {
+//   $('.hamburger-menu').fadeIn('fast');
+// });
+// $('.fa-times').dblclick(function() {
+//   $('.hamburger-menu').fadeOut(3000);
+// });
+
+//mouseenter e click
+// $('.fa-bars').mouseenter(function() {
+//   $('.hamburger-menu').fadeIn('slow');
+// });
+// $( '.fa-times' ).click(function() {
+//   disappear('.hamburger-menu');
+// });
+
+// mouseenter su due elementi per aprire e chiudere (funziona se la x non fosse sopra le barre,ma in questo caso lampeggia)
+// $('.fa-bars').mouseenter(function() {
+//   $('.hamburger-menu').fadeIn(2000);
+// });
+// $('.fa-times').mouseenter(function() {
+//   $('.hamburger-menu').fadeOut(3000);
+// });
