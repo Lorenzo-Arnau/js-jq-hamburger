38e83ca4b6737a8595b76569dfe626c16d458535
Lorenzo-Arnau <skyline03@hotmail.it>
2021-01-12 14:44:34 +0100

    update with better identifiers

--- main.js
+++ main.js
@@ -6,42 +6,42 @@
 // }
 
 // un click solo senza funzioni
-// $( '.fa-bars' ).click(function() {
-//  $('.hamburger-menu').show();
-// });
-// $( '.fa-times' ).click(function() {
-//  $('.hamburger-menu').hide();
-// });
+$( '.header-right a' ).click(function() {
+ $('.hamburger-menu').show();
+});
+$( '.close' ).click(function() {
+ $('.hamburger-menu').hide();
+});
 
 
 // un click solo con funzioni
-// $( '.fa-bars' ).click(function() {
+// $( '.header-right a' ).click(function() {
 //   appear('.hamburger-menu');
 // });
-// $( '.fa-times' ).click(function() {
+// $( '.close' ).click(function() {
 //   disappear('.hamburger-menu');
 // });
 
 //due click
-// $('.fa-bars').dblclick(function() {
+// $('.header-right a').dblclick(function() {
 //   $('.hamburger-menu').fadeIn('fast');
 // });
-// $('.fa-times').dblclick(function() {
+// $('.close').dblclick(function() {
 //   $('.hamburger-menu').fadeOut(3000);
 // });
 
 //mouseenter e click
-// $('.fa-bars').mouseenter(function() {
+// $('.header-right a').mouseenter(function() {
 //   $('.hamburger-menu').fadeIn('slow');
 // });
-// $( '.fa-times' ).click(function() {
+// $( '.close' ).click(function() {
 //   disappear('.hamburger-menu');
 // });
 
 // mouseenter su due elementi per aprire e chiudere (funziona se la x non fosse sopra le barre,ma in questo caso lampeggia)
-// $('.fa-bars').mouseenter(function() {
+// $('.header-right a').mouseenter(function() {
 //   $('.hamburger-menu').fadeIn(2000);
 // });
-// $('.fa-times').mouseenter(function() {
+// $('.close').mouseenter(function() {
 //   $('.hamburger-menu').fadeOut(3000);
 // });
